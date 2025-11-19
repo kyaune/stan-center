7e4a632b0358e4fa4f7e547cc82e9c5aad6349a4
stas <kyaune@gmail.com>
2025-11-20 03:49:26 +0700

    added gh pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+    base: '/stan-center/',
+    plugins: [
     vue(),
     vueDevTools(),
   ],
